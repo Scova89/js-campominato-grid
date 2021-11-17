7e6066cc676b776d11f2302c5aa6765ffe1dc8c3
Francesco <scovascova@gmail.com>
2021-11-17 16:12:14 +0100

    quadratini inseriti con la soluzione createElement

--- js/script.js
+++ js/script.js
@@ -2,12 +2,31 @@ const container = document.querySelector('.container-easy');
 
 let items = '';
 
-for(i = 1; i <= 100; i++){
-    items += `
-    <div class="box-easy">${i}</div>
-    `;
+// soluzione con innerHTML
+// for(i = 1; i <= 100; i++){
+//     items += `
+//     <div class="box">${i}</div>
+//     `;
+// }
+
+// console.log(items);
+
+// container.innerHTML = items;
+
+
+// soluzione con createElement
+
+// creo la funzione universale per aggiungere i quadrati
+function addBox(){
+    const nodo = document.createElement('div');
+    nodo.classList.add('box');
+    return nodo;
 }
 
-console.log(items);
-
-container.innerHTML = items;
+for(i = 1; i <= 100; i++){
+    container.appendChild(addBox());
+    items += `
+      <div class="box easy">${i}</div>
+      `;
+    container.innerHTML = items;
+}
